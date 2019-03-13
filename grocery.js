ff7259abc89e25922b9cdd706f150d000d8a2d57
mlandgraf1 <42742611+mlandgraf1@users.noreply.github.com>
2019-03-13 14:46:44 -0500

    TypeScript first draft
    First attempt at creating a class in a TypeScript file and outputting that as HTML code

---
+++ grocery.js
@@ -0,0 +1,15 @@
+var Grocery = /** @class */ (function () {
+    function Grocery(name, quantity, price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+        this.itemInformation = name + ', ' + quantity + ', ' + price;
+    }
+    return Grocery;
+}());
+;
+var item1 = new Grocery("Donuts", 4, 3.29);
+var item2 = new Grocery("Apples", 2, 1.50);
+var item3 = new Grocery("Dr. Pepper 12 Pack", 1, 4.99);
+var itemList = [item1.itemInformation, item2.itemInformation, item3.itemInformation];
+document.body.innerHTML = itemList.toString();
---
+++ grocery.ts
@@ -0,0 +1,15 @@
+class Grocery {
+    itemInformation: string;
+    constructor(public name: string, public quantity: number, public price: number) {
+        this.itemInformation = name + ', ' + quantity + ', ' + price;
+    }
+};
+
+let item1 = new Grocery("Donuts", 4, 3.29);
+let item2 = new Grocery("Apples", 2, 1.50);
+let item3 = new Grocery("Dr. Pepper 12 Pack", 1, 4.99)
+
+var itemList = [item1.itemInformation, item2.itemInformation, item3.itemInformation];
+
+document.body.innerHTML = itemList.toString();
+
